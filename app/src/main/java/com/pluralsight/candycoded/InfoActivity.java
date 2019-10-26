62709fafa579dfce80819ff9f760c9678606cd9f
paballo.moloi <paballo.moloi@multichoice.co.za>
2019-10-26 17:40:35 +0200

    Added phone intent code

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -24,7 +24,7 @@ public class InfoActivity extends AppCompatActivity {
 
 
     }
-    
+
     public void createMapIntent(View view) {
         Uri uriAddress = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, uriAddress);
@@ -34,7 +34,10 @@ public class InfoActivity extends AppCompatActivity {
             startActivity(mapIntent);
         }
     }
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
+    }
 }
